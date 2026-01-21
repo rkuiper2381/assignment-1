16bca4012adafd23ede62891986ff34e896235de
rkuiper2381 <rbk789@protonmail.com>
2026-01-21 18:25:34 -0500

    Remove typing on num

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -5,7 +5,7 @@ import {Request, Response} from 'express';
 import fibonacci from "./fib";
 
 export default (req : Request, res : Response) => {
-  const { num : number } = req.params;
+  const { num } = req.params;
 
   const fibN = fibonacci(parseInt(num));
   let result = `fibonacci(${num}) is ${fibN}`;
